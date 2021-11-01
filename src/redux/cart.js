d51eb4b4c8b5c06867e17967fcf8fff13b989c5a
Erin Han <eshan6597@gmail.com>
2021-11-01 13:03:32 -0700

    Update cart action types

--- src/redux/cart.js
+++ src/redux/cart.js
@@ -3,14 +3,17 @@ import * as ActionTypes from './ActionTypes';
 export const Cart = ( state = {
     cart: []
 }, action) => {
+
     if (action.type === ActionTypes.UPDATE_CART) {
         let find = state.cart.find(cartItem => cartItem.id === action.itemId);
+
         if (find) {
             const newQuantity = find.quantity + action.quantity;
             const update = state.cart.map(cartItem => 
                 cartItem.id === action.itemId ? {...cartItem, quantity: newQuantity} 
                 : cartItem);
             return {...state, cart: update}
+            
         } else {
             const newCartItem = {
                 id: action.itemId,
@@ -18,6 +21,14 @@ export const Cart = ( state = {
             };
             return {...state, cart: this.state.cart.concat(newCartItem)}
         }
+
+    } else if (action.type === ActionTypes.REMOVE_CART_ITEM) {
+        const update = state.cart.filter(cartItem => cartItem.id !== action.itemId);
+        return {...state, cart: update}
+
+    } else if (action.type === ActionTypes.EMPTY_CART) {
+        return {...state, cart: []}
+
     } else {
         return state;
     }
